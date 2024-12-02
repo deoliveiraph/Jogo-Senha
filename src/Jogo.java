d6cfbb42fe8c0e53a2844121a4bb0f0a64703ac6
Pedro Henrique <166876735+deoliveiraph@users.noreply.github.com>
2024-12-02 20:06:17 -0300

    Agora com a opcao de jogar novamente

--- src/Jogo.java
+++ src/Jogo.java
@@ -9,14 +9,17 @@ public class Jogo {
     private final ManipuladorDeEntrada manipuladorDeEntrada = new ManipuladorDeEntrada(scanner, mapaDeCores);
 
     public void iniciar() {
-        exibirMensagemInicial();
-        mapaDeCores.exibirCoresDisponiveis();
+        boolean jogarNovamente;
+        do {
+            exibirMensagemInicial();
+            mapaDeCores.exibirCoresDisponiveis();
 
-        Map<Integer, String> corMap = mapaDeCores.criarMapaDeCores();
-        String[] senha = geradorDeSenha.gerarSenha(corMap);
-        String[][] matriz = new String[8][4];
+            Map<Integer, String> corMap = mapaDeCores.criarMapaDeCores();
+            String[] senha = geradorDeSenha.gerarSenha(corMap);
+            String[][] matriz = new String[8][4];
 
-        jogar(corMap, senha, matriz);
+            jogarNovamente = jogar(corMap, senha, matriz);
+        } while (jogarNovamente);
     }
 
     private void exibirMensagemInicial() {
@@ -28,25 +31,40 @@ public class Jogo {
                 "Agora e com voce Boa sorte na descoberta da senha e pense bem nas suas jogadas o maximo de tentativas sao 8\n");
     }
 
-    private void jogar(Map<Integer, String> corMap, String[] senha, String[][] matriz) {
+    private boolean jogar(Map<Integer, String> corMap, String[] senha, String[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
             System.out.println("Tentativa " + (i + 1) + ":");
             manipuladorDeEntrada.preencherTentativa(matriz[i]);
             String resultado = validadorDeTentativa.verificarTentativa(matriz[i], senha);
             System.out.println("Resultado da tentativa " + resultado);
             if (resultado.equals("4 pretos, 0 brancos")) {
-                System.out.println("Aeee, voce descobriu a senha");
-                return;
+                System.out.println("Aeee, voce descobriu a senha. Deseja jogar novamente? (sim/nao)");
+                return desejaJogarNovamente();
             }
         }
         exibirSenhaCorreta(senha);
+        System.out.println("putz, que pena quer mais uma chance? (sim/nao)");
+        return desejaJogarNovamente();
     }
 
     private void exibirSenhaCorreta(String[] senha) {
-        System.out.println("Voce nao conseguiu descobrir a senha A senha era ");
+        System.out.println("Voce nao conseguiu descobrir a senha. A senha era ");
         for (String cor : senha) {
             System.out.print(cor + " ");
         }
         System.out.println();
+    }
+
+    private boolean desejaJogarNovamente() {
+        while (true) {
+            String resposta = scanner.nextLine().trim().toLowerCase();
+            if (resposta.equals("sim")) {
+                return true;
+            } else if (resposta.equals("nao")) {
+                return false;
+            } else {
+                System.out.println("Resposta invalida. Por favor, digite 'sim' ou 'nao'.");
+            }
+        }
     }
 }
